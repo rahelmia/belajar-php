bd73ad4107c637a647d5b7aef1069439fddb8195
Alifia Farah Helmia <112841837+rahelmia@users.noreply.github.com>
2022-10-27 11:03:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hallo Selamat Datang, Mari menghitung <br>";
+
+$nama = "Farah";
+$umur = 21;
+
+echo "nama saya <Strong> $nama</strong>, saya berusia $umur tahun.<br>";
+
+$nama = "Helmia";
+$umur = 16;
+
+$selisihUmur = $umur-$umuradik ;
+echo "nama Adik saya <Strong> $nama</strong>, Adik saya berusia $umuradik tahun.<br> Selisih umur saya dan adik saya $selisih"
+
+?>
